44c9c1bc6acea763c46f7665e7ee077fbecb64c0
Ruthika <ruthirockstar2218@gmail.com>
2023-08-21 22:27:12 +0530

    Side Nav

--- cactii/src/components/Sidenav.jsx
+++ cactii/src/components/Sidenav.jsx
@@ -1,5 +1,7 @@
 import React, {useState} from 'react'
-import {AiOutlineMenu, AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineMenu, AiFillHome} from 'react-icons/ai'
+import {GiCactusPot} from 'react-icons/gi'
+import {BsFillPeopleFill} from 'react-icons/bs';
 const Sidenav = () => {
 
     const [nav,setNav] = useState(false)
@@ -11,18 +13,27 @@ const Sidenav = () => {
     <div className=''>      
         <AiOutlineMenu
         onClick={handleNav}
-        className="icon absolute top-4 right-4 z-[99]" //add md:hidden -_-
+        className="icon absolute top-4 right-4 z-[99] md:hidden" //add md:hidden -_-
         
         
-        color="#F2E7E0"
+        color="#E87B67"
         />
         { 
         nav ?(
             <div className="bg-[#FFD166] h-screen fixed flex flex-col z-20 w-full justify-center items-center">
-                <a href="#main" className=' justify-center flex w-[75%] rounded-md items-center m-2 p-4 cursor-pointer hover:scale-100 ease-in duration-300'>
-                    <AiOutlineHome size={20}/>
+                <a href="#main" className='border-2  border-dotted flex border-black  justify-center bg-[#E87B67]  w-[75%] rounded-md items-center m-2 p-4 cursor-pointer hover:scale-100 ease-in duration-300  '>
+                    <AiFillHome size={20}/>
                     <span className='pl-4'>Home</span>
                 </a>
+                <a href="#about" className='border-2 border-dotted flex border-black  justify-center bg-[#E87B67]  w-[75%] rounded-md items-center m-2 p-4 cursor-pointer hover:scale-100 ease-in duration-300  '>
+                    <GiCactusPot size={20}/>
+                    <span className='pl-4'>See Me</span>
+                </a>
+                <a href="#contact" className='border-2 border-dotted flex border-black  justify-center bg-[#E87B67]  w-[75%] rounded-md items-center m-2 p-4 cursor-pointer hover:scale-100 ease-in duration-300  '>
+                    <BsFillPeopleFill size={20}/>
+                    <span className='pl-4'>Contact Me</span>
+                </a>
+
             </div>
 
         ):(
@@ -30,7 +41,20 @@ const Sidenav = () => {
         )
         }
         
-            
+        <div className="md:block hidden fixed top-[25%] z-10">
+            <div className="flex flex-col">
+                <a href="#main" className='rounded-full shadow-lg bg-[#E87B67] m-2 p-4 cursor-pointer hover:scale-110 duration-300'>
+                    <AiFillHome/>
+                </a>
+                <a href="#main" className='rounded-full shadow-lg bg-[#E87B67] m-2 p-4 cursor-pointer hover:scale-110 duration-300'>
+                    <GiCactusPot/>
+                </a>
+                <a href="#main" className='rounded-full shadow-lg bg-[#E87B67] m-2 p-4 cursor-pointer hover:scale-110 duration-300'>
+                    <BsFillPeopleFill/>
+                </a>
+
+            </div>
+        </div>
         
     </div>
   )
